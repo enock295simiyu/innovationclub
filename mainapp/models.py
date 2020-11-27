b33be928410af4b9aa1ea8eb2aae3694c1b7ff14
Enock Simiyu <60577078+enock295simiyu@users.noreply.github.com>
2020-11-27 12:46:52 +0300

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
 
+
 class Leader(models.Model):
     name = models.ForeignKey(User,on_delete=models.CASCADE)
     course = models.CharField(max_length=100)
